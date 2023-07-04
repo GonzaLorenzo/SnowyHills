495835b374e1ea9615312c36e5c2194d114a4f69
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2023-07-04 06:32:32 -0300

    CurrentBoard Test

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,12 +9,22 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private GameObject _board;
     [SerializeField] private GameObject _frontBoard;
     [SerializeField] private GameObject _rearBoard;
+
+    [SerializeField] private GameObject _currentBoard;
+
     private Rigidbody _rb;
     private bool _isGrounded;
+    private bool _launchRaycast = true;
+
+    //BOOL DE TESTEO.
+    public bool hasLost = false;
+    //BOOL DE TESTEO.
 
     [SerializeField] private float _jumpForce;
-    [SerializeField] private float _rotationSpeed = 5f;
-    [SerializeField] private float _targetRotationY = 0f;
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _airRotationSpeed;
+    private float _targetRotationY = 0f;
+    private float _targetRotationX = 0f;
     [SerializeField] private float _boardAdjustSpeed;
     [SerializeField] private float _playerAdjustSpeed;
     
@@ -25,7 +35,9 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Debug")]
     public TextMeshProUGUI speedText;
+    public TextMeshProUGUI velocityText;
     public TextMeshProUGUI isGroundedText;
+    public TextMeshProUGUI hasLostText;
 
     void Start()
     {
@@ -35,7 +47,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, -transform.up, out hit, _raycastDistance, _terrainLayer))
+        if(Physics.Raycast(transform.position, -transform.up, out hit, _raycastDistance, _terrainLayer) && _launchRaycast)
         {
             //Board adjust to normal.
             Quaternion boardTargetRotation = Quaternion.FromToRotation(_board.transform.up, hit.normal) * _board.transform.rotation;
@@ -45,7 +57,11 @@ public class PlayerMovement : MonoBehaviour
             Quaternion playerTargetRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
             transform.rotation = Quaternion.Lerp(transform.rotation, playerTargetRotation, _playerAdjustSpeed * Time.deltaTime);
 
-            _isGrounded = true;
+            if(!_isGrounded)
+            {
+                CheckCurrentBoard();
+                _isGrounded = true;
+            }
         }
         else
         {
@@ -53,34 +69,113 @@ public class PlayerMovement : MonoBehaviour
         }
 
         speedText.text = "Speed: " + _rb.velocity.magnitude;
+        velocityText.text = "Velocity: " + _rb.velocity;
         isGroundedText.text = "Is Grounded: " + _isGrounded;
 
-        if(Input.GetKeyDown(KeyCode.Space) && _isGrounded)
+        if(_isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            StartCoroutine(StopRaycast());
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            _targetRotationY += _rotationSpeed * Time.deltaTime;
+            if(_isGrounded)
+            {
+                _targetRotationY += _rotationSpeed * Time.deltaTime;
+            }
+            else
+            {
+                _targetRotationY += _airRotationSpeed * Time.deltaTime;
+            }
+            
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            _targetRotationY -= _rotationSpeed * Time.deltaTime;
+            if(_isGrounded)
+            {
+                _targetRotationY -= _rotationSpeed * Time.deltaTime;
+            }   
+            else
+            {
+                _targetRotationY -= _airRotationSpeed * Time.deltaTime;
+            }
+            
         }
+        /* if(!_isGrounded && Input.GetKey(KeyCode.S)) Encontrar la forma de rotar en eje x en el aire.
+        {
+            _targetRotationX += _airRotationSpeed * Time.deltaTime;
+        }
+        if(!_isGrounded && Input.GetKey(KeyCode.W))
+        {
+            _targetRotationX -= _airRotationSpeed * Time.deltaTime;
+        } */
 
-        Quaternion targetRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, _targetRotationY, transform.rotation.eulerAngles.z);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * _rotationSpeed);
+        if(_isGrounded)
+        {
+            Quaternion targetRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, _targetRotationY, transform.rotation.eulerAngles.z);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * _rotationSpeed);
+        }
+        else
+        {
+            Quaternion targetRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, _targetRotationY, transform.rotation.eulerAngles.z);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * _airRotationSpeed);
+        }
+        
     }
 
     void FixedUpdate()
     {
         if (_isGrounded)
         {
-            Vector3 forwardDirection = _frontBoard.transform.forward;
+            //Vector3 forwardDirection = _frontBoard.transform.forward;
+            Vector3 forwardDirection = _currentBoard.transform.forward;
             float yVelocity = _rb.velocity.y;
             _rb.velocity = forwardDirection * _rb.velocity.magnitude;
             _rb.velocity = new Vector3(_rb.velocity.x, yVelocity, _rb.velocity.z);
+
+            //Probar agregar una fuerza hacia el forwardDirection. Para que sea mas pesado el giro.
         }
+    }
+
+    private void ChangeCurrentBoard()
+    {
+        if(_currentBoard = _frontBoard)
+        {
+            _currentBoard = _rearBoard;
+        }
+        else
+        {
+            _currentBoard = _frontBoard;
+        }
+    }
+
+    private void CheckCurrentBoard()
+    {
+        /* if(_frontBoard.transform.position.y < _rearBoard.transform.position.y)
+        {
+            _currentBoard = _frontBoard;
+        }
+        else
+        {
+            _currentBoard = _rearBoard;
+        } */
+
+        //Funciona mejor pero no es esto.
+        if(Vector3.Distance(_frontBoard.transform.position, _rb.velocity) < Vector3.Distance(_rearBoard.transform.position, _rb.velocity))
+        {
+            _currentBoard = _rearBoard;
+        }
+        else
+        {
+            _currentBoard = _frontBoard;
+        }
+    }
+
+    IEnumerator StopRaycast()
+    {
+        _launchRaycast = false;
+        yield return new WaitForSeconds(0.5f);
+        _launchRaycast = true;
     }
 }
