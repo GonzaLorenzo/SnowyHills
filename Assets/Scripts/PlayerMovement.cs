18d3fab4c4801f0016199f3a3878da19bcb373a8
GonzaLorenzo <gonzalodlorenzo@gmail.com>
2023-07-04 05:30:12 -0300

    SphereCollider & CharacterTest

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,10 +12,12 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody _rb;
     private bool _isGrounded;
 
+    [SerializeField] private float _jumpForce;
     [SerializeField] private float _rotationSpeed = 5f;
     [SerializeField] private float _targetRotationY = 0f;
     [SerializeField] private float _boardAdjustSpeed;
-    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _playerAdjustSpeed;
+    
 
     [Header("Raycast")]
     [SerializeField] private LayerMask _terrainLayer;
@@ -35,10 +37,13 @@ public class PlayerMovement : MonoBehaviour
         RaycastHit hit;
         if(Physics.Raycast(transform.position, -transform.up, out hit, _raycastDistance, _terrainLayer))
         {
+            //Board adjust to normal.
             Quaternion boardTargetRotation = Quaternion.FromToRotation(_board.transform.up, hit.normal) * _board.transform.rotation;
-            
             _board.transform.rotation = Quaternion.Lerp(_board.transform.rotation, boardTargetRotation, _boardAdjustSpeed * Time.deltaTime);
-            //transform.rotation = targetRotation;
+
+            //Player adjust to normal.
+            Quaternion playerTargetRotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+            transform.rotation = Quaternion.Lerp(transform.rotation, playerTargetRotation, _playerAdjustSpeed * Time.deltaTime);
 
             _isGrounded = true;
         }
@@ -73,31 +78,9 @@ public class PlayerMovement : MonoBehaviour
         if (_isGrounded)
         {
             Vector3 forwardDirection = _frontBoard.transform.forward;
-            float yVelocity = _rb.velocity.y; // Guardar la velocidad en el eje Y
+            float yVelocity = _rb.velocity.y;
             _rb.velocity = forwardDirection * _rb.velocity.magnitude;
-            _rb.velocity = new Vector3(_rb.velocity.x, yVelocity, _rb.velocity.z); // Asignar la velocidad en el eje Y nuevamente
+            _rb.velocity = new Vector3(_rb.velocity.x, yVelocity, _rb.velocity.z);
         }
-    }
-
-    public Vector3 ComputeTorque(Quaternion desiredRotation){
-        //q will rotate from our current rotation to desired rotation
-        Quaternion q = desiredRotation * Quaternion.Inverse(transform.rotation);
-        //convert to angle axis representation so we can do math with angular velocity
-        Vector3 x;
-        float xMag;
-        q.ToAngleAxis (out xMag, out x);
-        x.Normalize ();
-        //w is the angular velocity we need to achieve
-        Vector3 w = x * xMag * Mathf.Deg2Rad / Time.fixedDeltaTime;
-        w -= _rb.angularVelocity;
-        //to multiply with inertia tensor local then rotationTensor coords
-        Vector3 wl = transform.InverseTransformDirection (w);
-        Vector3 Tl;
-        Vector3 wll = wl;
-        wll = _rb.inertiaTensorRotation * wll;
-        wll.Scale(_rb.inertiaTensor);
-        Tl = Quaternion.Inverse(_rb.inertiaTensorRotation) * wll;
-        Vector3 T = transform.TransformDirection (Tl);
-        return T;
     }
 }
